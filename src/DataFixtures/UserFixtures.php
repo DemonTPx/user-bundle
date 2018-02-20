1e3d84579f00a3e563df44e0ded3f95342fa306c
Bert Hekman <demontpx@gmail.com>
2018-02-20 22:54:55 +0100

    Move and upgrade fixture and tag it appropriately

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -1,24 +1,18 @@
 <?php
 
-namespace Demontpx\UserBundle\DataFixtures\ORM;
+namespace Demontpx\UserBundle\DataFixtures;
 
 use Demontpx\UserBundle\Entity\User;
-use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * @copyright 2014 Bert Hekman
+ * @copyright 2018 Bert Hekman
  */
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+class UserFixtures extends Fixture
 {
     /** @var ObjectManager */
     private $manager;
-
-    /** @var ContainerInterface */
-    private $container;
 
     public function load(ObjectManager $manager)
     {
@@ -47,15 +41,5 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
         $this->manager->persist($user);
 
         return $user;
-    }
-
-    public function getOrder()
-    {
-        return 10;
-    }
-
-    public function setContainer(ContainerInterface $container = null)
-    {
-        $this->container = $container;
     }
 }
